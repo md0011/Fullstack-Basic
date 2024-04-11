526ee17469b033e4c036c69223f0017a40d7463d
md0011 <mayurd0303@gmail.com>
2024-04-11 15:14:40 +0530

    Connect Frontend with Backend

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,15 @@ import express from "express";
 
 const app = express();
 
+// Serve frontend:
+// app.use(express.static('dist'))
+
 app.get('/', (req, res) => {
     res.send('Server is ready!');
 })
 
 // Get a list of 5 jokes
-app.get('/jokes', (req, res) => {
+app.get('/api/jokes', (req, res) => {
     const jokes = [
         {
             id: 1,
