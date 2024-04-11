4f3bb44ee22e3a6038a6aff1dc4c5a8828353b7c
md0011 <mayurd0303@gmail.com>
2024-04-11 13:11:32 +0530

    write a backend

---
+++ backend/server.js
@@ -0,0 +1,45 @@
+import express from "express";
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Server is ready!');
+})
+
+// Get a list of 5 jokes
+app.get('/jokes', (req, res) => {
+    const jokes = [
+        {
+            id: 1,
+            title: 'A Joke',
+            content: 'This is first joke'
+        },
+        {
+            id: 2,
+            title: 'B Joke',
+            content: 'This is second joke'
+        },
+        {
+            id: 3,
+            title: 'C Joke',
+            content: 'This is third joke'
+        },
+        {
+            id: 4,
+            title: 'D Joke',
+            content: 'This is fourth joke'
+        },
+        {
+            id: 5,
+            title: 'E Joke',
+            content: 'This is fifth joke'
+        }
+    ]
+    res.send(jokes);
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Serve at http://localhost:${port}`);
+})
